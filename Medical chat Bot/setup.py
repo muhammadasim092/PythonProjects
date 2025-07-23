83eb6f109e014b8ee74fbe2055043240d743a081
Muhammad Asim <97046562+muhammadasim092@users.noreply.github.com>
2025-07-23 23:54:17 +0500

    medicalchatbot

---
+++ Medical chat Bot/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="medical-chat-bot",
+    version="0.1",
+    author= "Asim",
+    author_email= "asimmughal69@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+        
+    ]
+)
